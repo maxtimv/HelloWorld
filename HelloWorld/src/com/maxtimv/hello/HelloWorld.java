07f35c22f2a4a09ee088d54678bb954dcb074ebb
Maxim Timofeev <maxtimv@gmail.com>
2013-11-04 14:09:00 -0500

    First commit

---
+++ HelloWorld/src/com/maxtimv/hello/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.maxtimv.hello;
+
+/**
+ * @author Maxim Timofeev
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ HelloWorld/src/com/maxtimv/hello/Test.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.maxtimv.hello;
+
+/**
+ * @author Maxim Timofeev
+ *
+ */
+public class Test {
+	private String name;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	private int age;
+
+}
